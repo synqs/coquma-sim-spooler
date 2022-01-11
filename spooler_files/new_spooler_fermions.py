65b7791224340b6d932f8b0b14283765c95d3cea
Rohit Prasad Bhatt <www.rohitprasadbhatt@gmail.com>
2022-01-11 11:39:31 +0100

    These tests run but might be faulty

--- spooler_files/new_spooler_fermions.py
+++ spooler_files/new_spooler_fermions.py
@@ -152,48 +152,6 @@ def check_json_dict(json_dict):
             break
     return err_code.replace("\n", ".."), exp_ok
 
-
-# def nested_kronecker_product(a):
-#     """putting together a large operator from a list of matrices.
-
-#     Provide an example here.
-
-#     Args:
-#         a (list): A list of matrices that can connected.
-
-#     Returns:
-#         array: An matrix that operates on the connected Hilbert space.
-#     """
-#     if len(a) == 2:
-#         return np.kron(a[0], a[1])
-#     else:
-#         return np.kron(a[0], nested_kronecker_product(a[1:]))
-
-
-# def jordan_wigner_transform(j: int, lattice_length: int) -> np.array:
-#     """
-#     Builds up the fermionic operators in a 1D lattice.
-#     For details see : https://arxiv.org/abs/0705.1928
-
-#     Args:
-#         j : site index
-#         lattice_length :  how many sites does the lattice have ?
-
-#     Returns:
-#         psi_x: the field operator of creating a fermion on size j
-#     """
-#     p_arr = np.array([[0, 1], [0, 0]])
-#     z_arr = np.array([[1, 0], [0, -1]])
-#     id_arr = np.eye(2)
-#     operators = []
-#     for dummy in range(j):
-#         operators.append(z_arr)
-#     operators.append(p_arr)
-#     for dummy in range(lattice_length - j - 1):
-#         operators.append(id_arr)
-#     return nested_kronecker_product(operators)
-
-
 def create_memory_data(shots_array, exp_name, n_shots):
     """
     The function to create memeory key in results dictionary
@@ -223,30 +181,29 @@ def gen_circuit(json_dict):
     exp_name = next(iter(json_dict))
     ins_list = json_dict[next(iter(json_dict))]["instructions"]
     load_inds = []
-    for ins in ins_list:
-        if ins[0]=='load':
+    for inst in ins_list:
+        if inst[0]=="load":
             load_inds.append(inst[1][0])
     n_shots = json_dict[next(iter(json_dict))]["shots"]
     if "seed" in json_dict[next(iter(json_dict))]:
         np.random.seed(json_dict[next(iter(json_dict))]["seed"])
-    num_tweezers = 4  # length of the tweezer array
-    # Nstates = 2 ** (2 * num_tweezers)
+    num_tweezers = int(NUM_WIRES/2)  # length of the tweezer array
 
     # calculate relevant parameters
     lattice_length = 2 * num_tweezers
     Nstates = 2 ** (lattice_length)
-    psi = 1j * np.zeros(Nstates)
+    psi = np.zeros(lattice_length)# important do not convert to complex yet!
     psi[load_inds] = 1.
     num_particles = len(load_inds)
     S_z = psi[0::2]-1*psi[1::2]
-    S_z = 0.5*np.sum(S_z.real)
+    S_z = 0.5*np.sum(S_z)# np.sum(S_z.real)
 
     # #### Build entire Fock Space basis
     # First in decimal representation
 
     dec_basis = np.arange(Nstates)
     # Now in binary representation.
-    # See https://stackoverflow.com/questions/21918267/convert-decimal-range-to-numpy-array-with-each-bit-being-an-array-element
+    # See https://stackoverflow.com/a/21919812
 
     bin_basis = dec_basis[:,None] >> np.arange(2*num_tweezers)[::-1] & 1
     bin_basis = bin_basis.astype('int32')# Max 16 tweezers = 32 bits
@@ -264,8 +221,13 @@ def gen_circuit(json_dict):
     req_sect_sub_space = req_num_hil_space[S_z_hil_space==S_z]
     req_sect_sub_space = req_sect_sub_space*1.0 #important, converting sector basis to float
 
-    assert req_sect_sub_space.size>0, 'Empty sector' # Not really required. It can't be an empty sector as user choose a state there.
+    assert req_sect_sub_space.size>0, 'Empty sector' # Not really required but keep it. It can't be an empty sector as user choose a state there.
 
+    # Invert the order of tweezers because we start counting from left but until now this code was starting from right.
+    req_sect_sub_space = np.concatenate(np.split(req_sect_sub_space,num_tweezers,axis=1)[::-1],axis=1)
+    # now modify psi to get it in the basis of this sector
+    psi = 1.0*np.all(req_sect_sub_space==psi,axis=1)
+    psi = psi+0j
     # ### Since now we have the basis for our sector, lets use it to build different gate generators
 
     # #### Build hopping matrix
@@ -277,11 +239,11 @@ def gen_circuit(json_dict):
         diff_indices = np.where(test!=0.)[0]
         if diff_indices.size!=2:
             return hop_allowed
-        if diff_indices[1]-diff_indices[0]!=2:
+        if np.abs(diff_indices[1]-diff_indices[0])!=2:
             return hop_allowed
         if link!=None:
             tweezers_inds = diff_indices//2
-            if link != tweezers_inds[1]:
+            if link != np.min(tweezers_inds):
                 return hop_allowed
         hop_allowed = True
         return hop_allowed
@@ -316,70 +278,42 @@ def gen_circuit(json_dict):
     # Reasoning : phase matrix is digonal and non-zero for those states which have atleast one atom on the site where phase gate acts.
     list_phase_arrs =[]
 
-    for i in range(num_tweezers):
-        site_occups = req_sect_sub_space[:,2*i:2*i+2]
-        site_occups = np.sum(site_occups, axis=1)
+    for i in range(lattice_length):
+        site_occups = req_sect_sub_space[:,i]
         list_phase_arrs.append(np.diag(site_occups))
 
     ##############################################################
     ##############################################################
-
-    # loweringOp = []
-    # for i in range(lattice_length):
-    #     loweringOp.append(jordan_wigner_transform(i, lattice_length))
-
-    # number_operators = []
-    # for i in range(lattice_length):
-    #     number_operators.append(loweringOp[i].T.conj().dot(loweringOp[i]))
-    # # interaction Hamiltonian
-    # Hint = 0 * number_operators[0]
-    # for ii in range(l):
-    #     spindown_ind = 2 * ii
-    #     spinup_ind = 2 * ii + 1
-    #     Hint += number_operators[spindown_ind].dot(number_operators[spinup_ind])
-
-    # # work our way through the instructions
-    # psi = 1j * np.zeros(Nstates)
-    # psi[0] = 1
-
     measurement_indices = []
     shots_array = []
     for i in range(len(ins_list)):
         inst = ins_list[i]
         if inst[0] == "load":
             continue
-            # latt_ind = inst[1][0]
-            # psi = np.dot(loweringOp[latt_ind].T, psi)
-        if inst[0] == "hop":
+        if inst[0] == "fhop":
             # the first two indices are the starting points
             # the other two indices are the end points
             hop_inds = inst[1]
+            assert hop_inds[0]//2 == hop_inds[1]//2, 'First two wire inds are wrong'
+            assert hop_inds[2]//2 == hop_inds[3]//2, 'Last two wire inds are wrong'
             start_tweezer = hop_inds[0]//2
             destination_tweezer = hop_inds[2]//2
-            assert start_tweezer == hop_inds[1]//2, 'Wrong hop inds'
-            assert destination_tweezer == hop_inds[3]//2, 'Wrong hop inds'
+            assert np.abs(destination_tweezer-start_tweezer) == 1, 'Only nearest neighbour hop allowed'
             link = start_tweezer
             if link>destination_tweezer:
                 link = destination_tweezer
             theta = inst[2][0]
             Hhop = list_J_arrs[link]
-            # couple
-            # Hhop = loweringOp[latt_ind[0]].T.dot(loweringOp[latt_ind[2]]) + loweringOp[
-            #     latt_ind[2]
-            # ].T.dot(loweringOp[latt_ind[0]])
-            # Hhop += loweringOp[latt_ind[1]].T.dot(loweringOp[latt_ind[3]]) + loweringOp[
-            #     latt_ind[3]
-            # ].T.dot(loweringOp[latt_ind[1]])
             Uhop = expm(-1j * theta * Hhop)
             psi = np.dot(Uhop, psi)
-        if inst[0] == "int":
+        if inst[0] == "fint":
             # the first two indices are the starting points
             # the other two indices are the end points
             theta = inst[2][0]
             Uint = expm(-1j * theta * U_arr)
             # theta = inst[2][0]
             psi = np.dot(Uint, psi)
-        if inst[0] == "phase":
+        if inst[0] == "fphase":
             # the first two indices are the starting points
             # the other two indices are the end points
             Hphase = 0 * list_phase_arrs[0]
@@ -394,21 +328,12 @@ def gen_circuit(json_dict):
     # first convert the result of reduced subspace to full space.
     # only give back the needed measurements
     if measurement_indices:
-        probs = np.zeros(Nstates)
-        original_states = req_sect_sub_space.dot(1 << np.arange(req_sect_sub_space.shape[-1] - 1, -1, -1))# See https://stackoverflow.com/a/15506055
-        probs[original_states] = np.abs(psi) ** 2
-        resultInd = np.random.choice(np.arange(Nstates), p=probs, size=n_shots)
+        probs = np.abs(psi) ** 2
+        resultInd = np.random.choice(np.arange(req_sect_sub_space.shape[0]), p=probs, size=n_shots)
 
         measurements = np.zeros((n_shots, len(measurement_indices)), dtype=int)
         for j in range(n_shots):
-            measurements[j] = bin_basis[resultInd[j]][measurement_indices]
-            # result = np.zeros(Nstates)
-            # result[resultInd[jj]] = 1
-
-            # for ii, ind in enumerate(measurement_indices):
-            #     observed = number_operators[ind].dot(result)
-            #     observed = observed.dot(result)
-            #     measurements[jj, ii] = int(observed)
+            measurements[j] = req_sect_sub_space[resultInd[j]][measurement_indices]
         shots_array = measurements.tolist()
 
     #print("done calc")
---
+++ tests/test_new_fermion.py
@@ -0,0 +1,224 @@
+"""
+Test module for the spooler_fermion.py file.
+"""
+
+import sys
+import os
+import numpy as np
+
+# pylint: disable=C0413, E0401
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "spooler_files"))
+from new_spooler_fermions import check_json_dict, gen_circuit  # *
+
+
+def run_json_circuit(json_dict, job_id):
+    """
+    A support function that executes the job.
+
+    Args:
+        json_dict: the job dict that will be treated
+        job_id: the number of the job
+
+    Returns:
+        the results dict
+    """
+    result_dict = {
+        "backend_name": "synqs_fermionic_tweezer_simulator",
+        "backend_version": "0.0.1",
+        "job_id": job_id,
+        "qobj_id": None,
+        "success": True,
+        "status": "finished",
+        "header": {},
+        "results": [],
+    }
+    err_msg, json_is_fine = check_json_dict(json_dict)
+    assert json_is_fine is True, "Failed JSON sanity check : " + err_msg
+    if json_is_fine:
+        for exp in json_dict:
+            exp_dict = {exp: json_dict[exp]}
+            # Here we
+            result_dict["results"].append(gen_circuit(exp_dict))
+
+    return result_dict
+
+
+###########################
+###########################
+# __Put all tests below__#
+###########################
+###########################
+
+
+def test_wire_order():
+    """
+    Test that the wire_order is properly working.
+    """
+
+    # first submit the job
+    job_payload = {
+        "experiment_0": {
+            "instructions": [
+                ["load", [7], []],
+                ["load", [2], []],
+                ["measure", [2], []],
+                ["measure", [6], []],
+                ["measure", [7], []],
+            ],
+            "num_wires": 8,
+            "shots": 4,
+            "wire_order": "sequential",
+        },
+    }
+
+    job_id = 1
+    try:
+        dummy = run_json_circuit(job_payload, job_id)
+    except AssertionError as ass_err:
+        print("Sucessfully triggered AssertionError", str(ass_err))
+
+
+def test_load_gate():
+    """
+    Test that the loading is properly working.
+    """
+
+    # first submit the job
+    job_payload = {
+        "experiment_0": {
+            "instructions": [
+                ["load", [7], []],
+                ["load", [2], []],
+                ["measure", [2], []],
+                ["measure", [6], []],
+                ["measure", [7], []],
+            ],
+            "num_wires": 8,
+            "shots": 4,
+            "wire_order": "interleaved",
+        },
+    }
+
+    job_id = 1
+    data = run_json_circuit(job_payload, job_id)
+    print(data)
+
+    shots_array = data["results"][0]["data"]["memory"]
+    assert data["job_id"] == 1, "job_id got messed up"
+    assert len(shots_array) > 0, "shots_array got messed up"
+    assert shots_array[0] == "1 0 1", "shots_array got messed up"
+
+
+def test_hop_gate():
+    """
+    Test that the hopping is properly working.
+    """
+
+    # first submit the job
+    job_payload = {
+        "experiment_0": {
+            "instructions": [
+                ["load", [0], []],
+                ["load", [1], []],
+                ["fhop", [0, 1, 2, 3], [np.pi / 2]],
+                ["measure", [0], []],
+                ["measure", [1], []],
+                ["measure", [2], []],
+                ["measure", [3], []],
+            ],
+            "num_wires": 8,
+            "shots": 4,
+            "wire_order": "interleaved",
+        },
+    }
+
+    job_id = 1
+    data = run_json_circuit(job_payload, job_id)
+    print(data)
+
+    shots_array = data["results"][0]["data"]["memory"]
+    assert data["job_id"] == 1, "job_id got messed up"
+    assert len(shots_array) > 0, "shots_array got messed up"
+    assert shots_array[0] == "0 0 1 1", "shots_array got messed up"
+
+
+def test_phase_gate():
+    """
+    Test that the phase gate is properly working.
+    """
+
+    # first submit the job
+    job_payload = {
+        "experiment_0": {
+            "instructions": [
+                ["load", [0], []],
+                ["fhop", [0, 1, 2, 3], [np.pi / 4]],
+                ["fphase", [4, 5], [np.pi]],
+                ["fhop", [0, 1, 2, 3], [np.pi / 4]],
+                ["measure", [0], []],
+                ["measure", [1], []],
+                ["measure", [2], []],
+            ],
+            "num_wires": 8,
+            "shots": 2,
+            "wire_order": "interleaved",
+        },
+    }
+
+    job_id = 1
+    data = run_json_circuit(job_payload, job_id)
+    print(data)
+
+    shots_array = data["results"][0]["data"]["memory"]
+    assert data["job_id"] == 1, "job_id got messed up"
+    assert len(shots_array) > 0, "shots_array got messed up"
+    assert shots_array[0] == "0 0 1", "shots_array got messed up"
+
+
+def test_seed():
+    """
+    Test that the hopping is properly working.
+    """
+
+    # first submit the job
+    job_payload = {
+        "experiment_0": {
+            "instructions": [
+                ["load", [0], []],
+                ["load", [1], []],
+                ["fhop", [0, 1, 2, 3], [np.pi / 4]],
+                ["measure", [0], []],
+                ["measure", [1], []],
+                ["measure", [2], []],
+                ["measure", [3], []],
+            ],
+            "num_wires": 8,
+            "shots": 4,
+            "wire_order": "interleaved",
+            "seed": 12345,
+        },
+        "experiment_1": {
+            "instructions": [
+                ["load", [0], []],
+                ["load", [1], []],
+                ["fhop", [0, 1, 2, 3], [np.pi / 4]],
+                ["measure", [0], []],
+                ["measure", [1], []],
+                ["measure", [2], []],
+                ["measure", [3], []],
+            ],
+            "num_wires": 8,
+            "shots": 4,
+            "wire_order": "interleaved",
+            "seed": 12345,
+        },
+    }
+
+    job_id = 1
+    data = run_json_circuit(job_payload, job_id)
+
+    shots_array_1 = data["results"][0]["data"]["memory"]
+    shots_array_2 = data["results"][1]["data"]["memory"]
+    assert data["job_id"] == 1, "job_id got messed up"
+    assert len(shots_array_1) > 0, "shots_array got messed up"
+    assert shots_array_1 == shots_array_2, "seed got messed up"
